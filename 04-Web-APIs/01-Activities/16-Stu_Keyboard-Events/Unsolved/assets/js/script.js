e26460d11ae47c327b78f84d7e8b06d7be97f235
shelldan <shelldanlee@gmail.com>
2022-08-21 22:12:03 -0500

    update

--- 04-Web-APIs/01-Activities/16-Stu_Keyboard-Events/Unsolved/assets/js/script.js
+++ 04-Web-APIs/01-Activities/16-Stu_Keyboard-Events/Unsolved/assets/js/script.js
@@ -1,5 +1,20 @@
-const myBtn = document.getElementById("myBtn")
+function keydownAction(event) {
+  // The key property holds the value of the key press
+  var keyPress = event.key;
+  // The code property holds the key's code 
+  var keyCode = event.code;
+  // Updates content on page
+  document.querySelector("#key").textContent = keyPress;
+  document.querySelector("#code").textContent = keyCode;
+  document.querySelector("#status").textContent = "KEYDOWN Event";
+  console.log(event);
+}
 
-myBtn.addEventListener("click", function(e){
-  console.log(e);
-})
+function keyupAction() {
+  // Updates event to KEYUP Event when key is released
+  document.querySelector("#status").innerHTML = "KEYUP Event";
+}
+// Adds listener for keydown event
+document.addEventListener("keydown", keydownAction); //keydown arrow
+// Adds listener for keyup event
+document.addEventListener("keyup", keyupAction); //keyup arrow 
